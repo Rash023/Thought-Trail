276d22a151cdeefedeb1ad53599a4707bedf185d
Rash023 <rashidmazhar0507@gmail.com>
2024-05-07 23:31:05 +0530

    Initial Commit

---
+++ src/main/java/com/rash/thoughtTrail/controller/HealthCheck.java
@@ -0,0 +1,13 @@
+package com.rash.thoughtTrail.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+    @GetMapping("/health-check")
+    public String healthCheck(){
+        return "OK";
+
+    }
+}
---
+++ src/test/java/com/rash/thoughtTrail/ThoughtTrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rash.thoughtTrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThoughtTrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
